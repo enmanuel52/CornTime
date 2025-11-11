db2c06d07187b8fa7d9f23c6e0ac9559e7a15b27
enmanuel <enmanueldelgado52@gmail.com>
2025-11-11 10:37:59 -0300

    Hotfix: delete unused files

--- composeApp/src/commonMain/kotlin/com/enmanuelbergling/corntime/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.enmanuelbergling.corntime
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/com/enmanuelbergling/corntime/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.enmanuelbergling.corntime
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
