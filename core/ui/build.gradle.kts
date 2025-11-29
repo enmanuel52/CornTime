0f060ef49969b5d6bf09aa4ec7a545c2cb7d9a07
enmanuel <enmanueldelgado52@gmail.com>
2025-11-28 21:34:55 -0300

    Fix: org.jetbrains.compose.resources.MissingResourceException for android

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -27,6 +27,8 @@ kotlin {
         }.configure {
             instrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         }
+
+        experimentalProperties["android.experimental.kmp.enableAndroidResources"] = true
     }
 
     // For iOS targets, this is also where you should
